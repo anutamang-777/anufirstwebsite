78a89e91e9932de457735f18b0207704c762e779
nirmalkoirala7 <koiralanirmal7@gmail.com>
2021-12-22 16:38:43 +1100

    fixed navbar

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Download from './components/downloads/Download';
+import Faq from './components/faq/Faq';
+import Features from './components/features/Features';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Subscribe from './components/subscribe/Subscribe';
+
+function App() {
+  return (
+    <main>
+      <header
+        className="header-bg">
+        <Navbar />
+        <Header />
+      </header>
+      <Features />
+      <Download />
+      <Subscribe />
+      <Faq />
+      <Footer />
+
+
+    </main>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="container navbar">
+           <div className="logo">
+               
+           </div>
+           <menu>
+
+           </menu>
+        </nav>
+    )
+}
+
+export default Navbar
